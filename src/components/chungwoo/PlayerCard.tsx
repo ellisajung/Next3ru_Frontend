deeee6454134fecf26e80eb3fb945c9c171e6023
chungwoo <cgd67@naver.com>
2024-07-08 15:20:49 +0900

    feat: 오늘의 선수 클릭시 모달창 구현

--- src/components/chungwoo/PlayerCard.tsx
+++ src/components/chungwoo/PlayerCard.tsx
@@ -5,14 +5,50 @@ import Image from "next/image";
 import Player from "@/store/Today-player";
 
 const getColorClass = (rating: number) => {
-  if (rating < 3.0) {
-    return "bg-red-500";
-  } else if (rating < 5.0) {
-    return "bg-orange-500";
-  } else if (rating < 8.0) {
-    return "bg-green-500";
-  } else {
+  if (rating < 2.0) {
+    return "bg-violet-500";
+  } else if (rating < 4.0) {
     return "bg-blue-500";
+  } else if (rating < 6.0) {
+    return "bg-green-500";
+  } else if (rating < 8.0) {
+    return "bg-orange-500";
+  } else {
+    return "bg-red-500";
+  }
+};
+
+const getImageSrc = (rating: number) => {
+  if (rating < 2.0) {
+    return "/images/번개.svg";
+  } else if (rating < 4.0) {
+    return "/images/비.svg";
+  } else if (rating < 6.0) {
+    return "/images/구름.svg";
+  } else if (rating < 8.0) {
+    return "/images/해구름.svg";
+  } else {
+    return "/images/해.svg";
+  }
+};
+
+const getImageTop = (imageSrc: String) => {
+  if (imageSrc === "/images/번개.svg") {
+    return "-37%";
+  } else if (imageSrc === "/images/비.svg") {
+    return "-37%";
+  } else {
+    return "-37%";
+  }
+};
+
+const getImageLeft = (imageSrc: String) => {
+  if (imageSrc === "/images/해구름.svg") {
+    return "72%";
+  } else if (imageSrc === "/images/구름.svg") {
+    return "72%";
+  } else {
+    return "75%";
   }
 };
 
@@ -23,40 +59,51 @@ const PlayerCard: React.FC<Player> = ({
   position,
   role,
   rating,
+  pcode,
   onClick,
+  changeinn,
 }) => {
+  const colorClass = getColorClass(rating);
+  const imageSrc = getImageSrc(rating);
+
   return (
     <div
       onClick={onClick}
-      className="relative my-4 rounded-custom border border-black p-1 bg-black w-122 h-166.87"
+      className={`relative my-4 p-1 rounded-[18px] border border-black bg-black `}
     >
-      <div className="relative">
-        <Image src={imageUrl} alt={name} width={113} height={151} />
+      <div className="relative w-40 h-44">
+        <Image src={imageUrl} alt={name} layout="fill" style={{ top: "-7%", left: "0%" }} />
+
         <div
-          className={`absolute px-1 rounded-custom text-lg ${getColorClass(rating)}`}
-          style={{ top: "-13%", left: "88%" }}
+          className={`absolute p-1 rounded-lg font-bold text-xl text-center w-12 h-8 ${colorClass}`}
+          style={{ top: "-10%", left: "90%" }}
         >
           {rating}
         </div>
         <div
-          className="bg-black text-white text-sm absolute px-1 "
+          className={`absolute w-24 h-16 `}
+          style={{ top: getImageTop(imageSrc), left: getImageLeft(imageSrc) }}
+        >
+          <Image src={imageSrc} alt="weather" layout="fill" />
+        </div>
+        <div
+          className="bg-black text-white text-lg absolute px-1"
           style={{ top: "0%", left: "0%" }}
         >
           {role}
         </div>
       </div>
       <div className="text-white relative overflow-hidden p-1 rounded-lg">
-        <div className="text-white-40 text-sm pl-1 flex justify-start ">No. {number}</div>
-
-        <div className="ml-4 ">
+        <div className="text-white-40 text-lg pl-1 flex justify-start ">No. {number}</div>
+        <div className="ml-4 text-lg">
           {name}
           <Image
             src="/images/logo.svg"
             alt="로고다"
-            width={41.37}
-            height={44.17}
+            width={51.37}
+            height={54.17}
             className="absolute"
-            style={{ top: "12%", left: "73%" }}
+            style={{ top: "30%", left: "80%" }}
           />
         </div>
       </div>
