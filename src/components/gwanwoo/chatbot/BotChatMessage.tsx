3d324727684832b34b9865f1fbeacff7dc47ee21
Gwanwoo Shin <tlsrhksdn23@gmail.com>
2024-07-22 10:32:26 +0900

    feat: 챗봇을 구성하는 컴포넌트들 생성

---
+++ src/components/gwanwoo/chatbot/BotChatMessage.tsx
@@ -0,0 +1,8 @@
+"use client";
+import Image from "next/image";
+
+const BotChatMessage = () => {
+  //styled component로 작성된 css tailwind로 변경해서 적용하기
+};
+
+export default BotChatMessage;
---
+++ src/components/gwanwoo/chatbot/MyCustomAvatar.tsx
@@ -0,0 +1,9 @@
+//본인 채팅 아바타 컴포넌트
+"use client";
+
+const MyCustomAvatar = () => {
+  //공백 반환
+  return <></>;
+};
+
+export default MyCustomAvatar;
