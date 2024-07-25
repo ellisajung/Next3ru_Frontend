55c49becc08a588464f28647d9fa9c85d7a1d1db
ellisa <ellisajung12@gmail.com>
2024-07-25 19:01:16 +0900

    chore: 패키지 및 설정 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    domains: ["wizzap.ktwiz.co.kr"],
+  },
+  transpilePackages: ["three"],
+};
 
 export default nextConfig;
