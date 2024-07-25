b2f2f8009079e18cb25dc203c35d0777cc57aba9
ellisa <ellisajung12@gmail.com>
2024-07-25 19:05:05 +0900

    feat, style: 스와이퍼 컴포넌트 추가, 기존 스와이퍼와 css 스타일 파일 분리하여 작성(선택자 에러 발생)

--- src/components/elisa/AreaNameSwiper.tsx
+++ src/components/elisa/AreaNameSwiper.tsx
@@ -5,12 +5,15 @@ import { FreeMode, Scrollbar, Mousewheel } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/scrollbar";
-import "@/styles/elisa.css";
+import "@/styles/elisa-copy.css";
 import { seatInfo } from "@/components/elisa/seatInfo";
 
 const AreaNameSwiper = () => {
   return (
-    <div className="area-name-swiper">
+    <div
+      id="area-name-swiper"
+      className="w-2/3 h-full flex items-center"
+    >
       <Swiper
         direction={"vertical"}
         slidesPerView={"auto"}
@@ -21,13 +24,11 @@ const AreaNameSwiper = () => {
       >
         {seatInfo.areas.map(({ area_name, area_color }) => (
           <SwiperSlide>
-            <div>
-              <div>
-                <div
-                  className="w-3 h-3"
-                  style={{ backgroundColor: area_color }}
-                ></div>
-              </div>
+            <div className="w-full flex justify-start items-center p-1">
+              <div
+                className="w-5 h-5 rounded border-solid border-2 hover:outline hover:outline-2 hover:outline-black dark:hover:outline-white mr-2"
+                style={{ backgroundColor: area_color }}
+              ></div>
               <p>{area_name}</p>
             </div>
           </SwiperSlide>
