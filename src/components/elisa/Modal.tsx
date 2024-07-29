9372bc3c9a8f603f77a470a2f2f8e04d7f47dd3a
ellisa <ellisajung12@gmail.com>
2024-07-29 11:47:21 +0900

    refact: Modal, AreaNameSwiper, ViewPicSwiper 컴포넌트 수정 (ViewPicSwiper 이미지 구역내 스와이핑 안되는 에러 미해결)

--- src/components/elisa/Modal.tsx
+++ src/components/elisa/Modal.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/shadcn-ui/button";
 import ViewPicSwiper from "./ViewPicSwiper";
-// import SwiperComponent from "@/components/elisa/Swiper";
-
+import { Card } from "../shadcn-ui/card";
+import "../../styles/elisa.css";
 interface IModalProps {
   setShowModal: (show: boolean) => void;
   info: any;
@@ -10,26 +10,27 @@ interface IModalProps {
 const Modal: React.FC<IModalProps> = ({ setShowModal, info }) => {
   return (
     <div id="modal">
-      <div id="modal-wrap">
-        <p>
-          {info?.name} 의 {info?.text} 구역입니다.
-        </p>
-        {/* 사진뷰 구역 시작 */}
-        <ViewPicSwiper zone="z-113" />
-        {/* 사진뷰 구역 끝 */}
-        {/* 평점 구역 시작 */}
-        {/* 평점 구역 끝 */}
-        {/* 리뷰 리스트 구역 시작 */}
-        {/* 리뷰 리스트 구역 끝 */}
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => setShowModal(false)}
-          className="text-gray-700"
-        >
-          닫기
-        </Button>
-      </div>
+      <Card>
+        <div id="modal-wrap">
+          <p>
+            {info?.name}의 {info?.text}구역입니다.
+          </p>
+          {/* 사진뷰 구역 시작 */}
+          <ViewPicSwiper zone="z-지니좌" />
+          {/* 사진뷰 구역 끝 */}
+          {/* 평점 구역 시작 */}
+          {/* 평점 구역 끝 */}
+          {/* 리뷰 리스트 구역 시작 */}
+          {/* 리뷰 리스트 구역 끝 */}
+          <Button
+            variant="secondary"
+            size="icon"
+            onClick={() => setShowModal(false)}
+          >
+            닫기
+          </Button>
+        </div>
+      </Card>
     </div>
   );
 };
