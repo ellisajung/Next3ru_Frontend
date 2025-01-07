a49e533dedd8850c787d1f424453e3ed4c36fb92
Elisa <125180849+ellisajung@users.noreply.github.com>
2025-01-08 02:50:25 +0900

    auth: 로그인/회원가입 이메일 검증 후 에러/성공 메시지 표시

--- src/app/(user)/actions.ts
+++ src/app/(user)/actions.ts
@@ -9,39 +9,42 @@ export async function signIn(formData: FormData) {
 
   const { data, error } = await supabase.auth.signInWithOtp({
     email: formData.get('email') as string,
-    // options: {
-    //   // set this to false if you do not want the user to be automatically signed up
-    //   shouldCreateUser: false,
-    //   emailRedirectTo: 'https://example.com/welcome',
-    // },
+    options: {
+      // set this to false if you do not want the user to be automatically signed up
+      shouldCreateUser: false,
+      // emailRedirectTo: 'https://example.com/welcome',
+    },
   })
 
-  if (error) {
-    redirect('/error')
-  }
-
-  revalidatePath('/', 'layout')
-  redirect('/')
   
+  if (error) {
+    const query = encodeURIComponent("존재하지 않는 이메일입니다.")
+    redirect(`/sign-in?error-message=${query}`)
+  }
+  
+  revalidatePath('/', 'layout')
+  
+  const query = encodeURIComponent("이메일로 전송된 링크를 확인해 주세요.")
+  redirect(`/sign-in?confirm-message=${query}`)  
 }
 
 export async function signUp(formData: FormData) {
   const supabase = await createClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
+  const { data, error } = await supabase.auth.signInWithOtp({
     email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+  })
 
-  const { error } = await supabase.auth.signUp(data)
-
+  
   if (error) {
-    redirect('/error')
+    const query = encodeURIComponent("유효한 이메일을 입력해 주세요.")
+    redirect(`/sign-up?error-message=${query}`)
   }
-
+  
+  
   revalidatePath('/', 'layout')
-  redirect('/')
+  
+  const query = encodeURIComponent("이메일로 전송된 링크를 확인해 주세요.")
+  redirect(`/sign-up?confirm-message=${query}`)
 }
 
--- src/app/(user)/sign-up/page.tsx
+++ src/app/(user)/sign-up/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Link from "next/link";
-import { Button, buttonVariants } from "@/components/shadcn-ui/button";
+import { Button } from "@/components/shadcn-ui/button";
 import {
   Card,
   CardContent,
@@ -11,113 +13,128 @@ import { Input } from "@/components/shadcn-ui/input";
 import { Label } from "@/components/shadcn-ui/label";
 import Image from "next/image";
 import { signUp } from "../actions";
+import { useSearchParams } from "next/navigation";
 
 const SignUpPage = () => {
+  const errorMsg = useSearchParams().get("error-message");
+  const confirmMsg = useSearchParams().get("confirm-message");
   return (
-    <Card className="w-96 mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-xl font-bold flex justify-center">
-          회원가입
-        </CardTitle>
-        <CardDescription className="flex justify-center">
-          회원가입 방식을 선택해 주세요.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="username">이름</Label>
-            <Input
-              id="username"
-              name="username"
-              type="text"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">이메일</Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <Button formAction={signUp}>이메일 회원가입</Button>
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t"></span>
+    <form>
+      <Card className="w-96 mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl font-bold flex justify-center">
+            회원가입
+          </CardTitle>
+          <CardDescription className="flex justify-center">
+            회원가입 방식을 선택해 주세요.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="username">이름</Label>
+              <Input
+                id="username"
+                name="username"
+                type="text"
+                required
+              />
             </div>
-            <div className="relative flex justify-center text-sm uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                또는
-              </span>
+            <div className="grid gap-2">
+              <Label htmlFor="email">이메일</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
             </div>
-          </div>
-          {/* <Link
+            {errorMsg && (
+              <div className="text-sm text-red-700">
+                {decodeURIComponent(errorMsg)}
+              </div>
+            )}
+            {confirmMsg && (
+              <div className="text-sm text-green-700">
+                {decodeURIComponent(confirmMsg)}
+              </div>
+            )}
+            <Button formAction={signUp}>이메일 회원가입</Button>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t"></span>
+              </div>
+              <div className="relative flex justify-center text-sm uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  또는
+                </span>
+              </div>
+            </div>
+            {/* <Link
             href="/"
             className={`bg-[#FEE600] ${buttonVariants({
               variant: "secondaryNone",
             })}`}
           > */}
-          <Link
-            href="/"
-            className="bg-[#FEE600] flex justify-center rounded-md"
-          >
-            <Image
-              src="/images/social-logos/kakao-narrow.png"
-              alt=""
-              width={160}
-              height={40}
-            />
-          </Link>
-          {/* <Link
+            <Link
+              href="/"
+              className="bg-[#FEE600] flex justify-center rounded-md"
+            >
+              <Image
+                src="/images/social-logos/kakao-narrow.png"
+                alt=""
+                width={160}
+                height={40}
+              />
+            </Link>
+            {/* <Link
             href="/"
             className={`bg-[#02C75A] ${buttonVariants({
               variant: "secondaryNone",
             })}`}
           > */}
-          <Link
-            href="/"
-            className="bg-[#02C75A] flex justify-center rounded-md"
-          >
-            <Image
-              src="/images/social-logos/naver.png"
-              alt=""
-              width={140}
-              height={40}
-            />
-          </Link>
-          {/* <Link
+            <Link
+              href="/"
+              className="bg-[#02C75A] flex justify-center rounded-md"
+            >
+              <Image
+                src="/images/social-logos/naver.png"
+                alt=""
+                width={140}
+                height={40}
+              />
+            </Link>
+            {/* <Link
             href="/"
             className={`bg-[#F2F2F2] ${buttonVariants({
               variant: "secondaryNone",
             })}`}
           > */}
-          <Link
-            href="/"
-            className="bg-[#F2F2F2] flex justify-center rounded-md"
-          >
-            <Image
-              src="/images/social-logos/google.svg"
-              alt=""
-              width={170}
-              height={40}
-            />
-          </Link>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          이미 계정이 있으신가요?&nbsp;
-          <Link
-            href="login"
-            className="underline font-semibold"
-          >
-            로그인하기
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+            <Link
+              href="/"
+              className="bg-[#F2F2F2] flex justify-center rounded-md"
+            >
+              <Image
+                src="/images/social-logos/google.svg"
+                alt=""
+                width={170}
+                height={40}
+              />
+            </Link>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            이미 계정이 있으신가요?&nbsp;
+            <Link
+              href="sign-in"
+              className="underline font-semibold"
+            >
+              로그인하기
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
   );
 };
 
